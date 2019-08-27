28444bec5a0bad71b70011a5d8e6f8832d8c6ff7
dhanraj404 <53806611+dhanraj404@users.noreply.github.com>
2019-08-27 09:10:24 +0530

    Update Infixtopostfix.c

--- Infixtopostfix.c
+++ Infixtopostfix.c
@@ -1,66 +1,104 @@
 #include<stdio.h>
+#include<conio.h>
+#include<string.h>
 #include<ctype.h>
-void push(char c,int *t,char s[20]);
-char pop(int *t,char s[20]);
-void main()
+
+int isoperator(char ch);
+char pop(char a[40],int* t);
+void push(char ele,char a[40],int* t);
+int precedence(char ch);
+
+char pop(char a[40],int* t)
 {
-	char stack[20];
-	int top = -1;
-	char exp[20];
-	char *e, x;
-	int check=0;
-	printf("Enter the expression :: ");
-	scanf("%s",exp);
-	e = exp;
-	printf("Postfix expression:: ");
-    	while(*e != '\0')
-	{
-		check=isalnum(*e);
-        	if(check==1)
-            		printf("%c",*e);
-        	else if(*e == '(')
-			push(*e,&top,stack);
-		else if(*e == ')')
-        	{
-			while((x = pop(&top,stack)) != '(')
-                		printf("%c", x);
-        	}
-		else
-		{
-            		while(priority(stack[top]) >= priority(*e))
-				printf("%c",pop(&top,stack));
-			push(*e,&top,stack);
-        	}
-        	e++;
-    	}
-    	while(top != -1)
-    	{
-		printf("%c",pop(&top,stack));
-    	}
-	printf("\n");
+    char g;
+    if(*t>-1)
+       {
+        g=a[*t];
+        *t-=1;
+       }
+   return g;
 }
-void push(char c,int *t,char s[20])
+void push(char ele,char a[40],int* t)
 {
-	if(*t==19)
-	{
-		printf("Stack Overflow");
-		return;
-	}
-	s[++(*t)] = c;
+    a[*t]=ele;
+    *t+=1;
 }
-char pop(int *t,char s[20])
+int isoperator(char ch)
 {
-	if((*t) == -1)
-		return -1;
-	else
-		return s[(*t)--];
+    if(ch=='/'||ch=='+'||ch=='-'||ch=='*'||ch=='^')
+         return 1;
+    else
+         return 0;
 }
-int priority(char x)
+int precedence(char ch)
 {
-	if(x == '(')
-		return 0;
-	if(x == '+' || x == '-')
-		return 1;
-	if(x == '*' || x == '/')
-		return 2;
+    switch(ch)
+    {
+    case '^':
+        return 3;
+        break;
+    case '*':
+    case '/':
+        return 2;
+        break;
+    case '+':
+    case '-':
+        return 1;
+        break;
+    default:
+        return 0;
+        break;
+    }
+}
+
+int main()
+{
+    char stack[40],infix[20],postfix[20];
+    printf("Enter the expression");
+    scanf("%s",infix);
+    int i=0,j=0,top=-1;
+    char sym;
+    while(infix[i]!='\0')
+    {
+        sym=infix[i];
+        if(sym=='(')
+        {
+            push(sym,stack,&top);
+        }
+        else if(isdigit(sym)||isalpha(sym))
+        {
+            postfix[j]=sym;
+            j++;
+        }
+        else if(isoperator(sym)==1)
+        {
+            if(precedence(sym)>precedence(top))
+            {
+                push(sym,stack,&top);
+            }
+            else
+                 postfix[j++]=sym;
+        }
+        else if(sym==')')
+        {
+
+            while(stack[top]!='(')
+              {
+                char x=pop(stack,&top);
+                postfix[j++]=x;
+              }
+              if(stack[top]=='(')
+              top--;
+        }
+        else
+             printf("Invalid Input");
+
+    }
+     while(top!=-1)
+     {
+         char v=pop(stack,&top);
+         postfix[j++]=v;
+     }
+     postfix[j]='\0';
+     puts(postfix);
 }
